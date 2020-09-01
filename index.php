418625c6d680ec538f79d787a8b46be899bc3609
Sean12mps <sean.michael.piettojo@gmail.com>
2020-09-01 18:13:30 +0700

    Add another column

--- index.php
+++ index.php
@@ -42,6 +42,21 @@
 				</div>
 			</div>
 		</div>
+		<div class="col-md-4">
+			<div class="wrapper-spell-group">
+				<div class="spell-group">
+					<h2>Others</h2>
+					<ul class="spell-group-list">
+						<li><?php print_card( array( 'img_url' => 'https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129659&type=card' ) ); ?></li>
+						<li><?php print_card( array( 'img_url' => 'https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129658&type=card' ) ); ?></li>
+						<li><?php print_card( array( 'img_url' => 'https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129651&type=card' ) ); ?></li>
+						<li><?php print_card( array( 'img_url' => 'https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129656&type=card' ) ); ?></li>
+						<li><?php print_card( array( 'img_url' => 'https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129655&type=card' ) ); ?></li>
+						<li><?php print_card( array( 'img_url' => 'https://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=129650&type=card' ) ); ?></li>
+					</ul>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 
